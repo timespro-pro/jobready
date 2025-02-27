01a0530102436171979d16585965d090816b9f51
timespro-pro <timesproa@gmail.com>
2025-02-28 01:16:33 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -97,5 +97,6 @@ if uploaded_file and job_id:
     if st.button("Save question and generate video link"):
         st.success("Video link will be sent shortly")
         st.image("https://source.unsplash.com/600x400/?interview,job", caption="Interview Process")
+
 else:
     st.warning("Please enter a Job ID and upload a PDF file.")
